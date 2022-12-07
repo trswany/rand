5e95994efb7b2defc17dea0d0801997c12c97a28
Tom Swanson <trswany@gmail.com>
2022-12-07 07:15:50 +0000

    Generate random pngs.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/png"
 	"log"
 	"math/rand"
 	"net/http"
@@ -11,6 +13,8 @@ import (
 func main() {
 	log.Println("Spooling up the rand() service.")
 	mux := http.NewServeMux()
+
+	// Generate a random uint64.
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		if req.URL.Path != "/" {
 			http.NotFound(w, req)
@@ -19,6 +23,23 @@ func main() {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		fmt.Fprintf(w, "%d", r.Uint64())
 	})
+
+	// Generate a random png.
+	mux.HandleFunc("/png", func(w http.ResponseWriter, req *http.Request) {
+		img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{240, 240}})
+		// Each pixel consists of 4 uint8's in R,G,B,A order.
+		for i := 0; i < len(img.Pix); i += 4 {
+			// Generate a single uint64 rand per pixel for max speed.
+			r := rand.New(rand.NewSource(time.Now().UnixNano())).Uint64()
+			img.Pix[i] = uint8(r)
+			img.Pix[i+1] = uint8(r >> 8)
+			img.Pix[i+2] = uint8(r >> 16)
+			// Always set full alpha.
+			img.Pix[i+3] = 255
+		}
+		png.Encode(w, img)
+	})
+
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
